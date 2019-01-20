015befbf79512df88f5113ac60452e7a6d001f56
asnawi saharuddin <asnawi@terato.com>
2019-01-20 14:56:59 +0800

    Update docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,4 @@ if [ ! -f /var/www/moodledata/id_rsa ]; then
     ssh-keygen -q -t rsa -N '' -f /var/www/moodledata/id_rsa
 fi
 
-# run php migration
-vendor/bin/phinx migrate
-
 /usr/bin/supervisord
